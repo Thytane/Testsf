980fbc20c1be76d5558b3a359167e1e5ea138cb1
ROUSSELIN Thierry <tro@cnes.fr>
2021-03-28 18:56:47 +0000

    Update install script

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 composer install
-php bin/phpunit
+php vendor/bin/phpunit
 php bin/console doctrine:database:create
 php bin/console make:migration
 php bin/console doctrine:migrations:migrate
